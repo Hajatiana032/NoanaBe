393f9897926a3ede50f4128ce15760a91277cbaa
Haja N-Lanto <nirilantohajatiana032@gmail.com>
2024-09-05 16:22:03 +0300

    mise en place d'une layout pour les utilisateurs

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/admin', name: 'app_user')]
+#[Route('/admin', name: 'admin_user')]
 class UserController extends AbstractController
 {
     #[Route('/utilisateurs', name: '')]
     public function index(): Response
     {
-        return $this->render('user/index.html.twig', [
+        return $this->render('admin/user/index.html.twig', [
             'currentMenu' => 'adminUser',
             'controller_name' => 'UserController',
         ]);
