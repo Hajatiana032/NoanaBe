49eafc2e09fb9c327a9999e8234187196f17a7c9
Haja N-Lanto <nirilantohajatiana032@gmail.com>
2024-09-23 22:44:14 +0300

    mise en place d'une système de recherche pour les produits du site

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Service\PaginationService;
 use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,6 +21,15 @@ class ProductController extends AbstractController
         return $this->render('product/show.html.twig', [
             'productsLike' => $productRepository->like($product->getCategory()->getId(), $product->getId()),
             'product' => $product
+        ]);
+    }
+
+    #[Route('?recherche', name: 'product_search', methods: ['GET'])]
+    public function search(Request $request, ProductRepository $productRepository, PaginationService $pagination)
+    {
+        return $this->render('product/search_results.html.twig', [
+            'query' => $request->query->get('q'),
+            'products' => $pagination->paginate($productRepository->search($request->query->get('q')), $request, 15)
         ]);
     }
 }
